6f46bfbc5fbafe86d57661ca7f1e92654544b19d
Fady Zakaria <fady.zakaria93@gmail.com>
2022-02-25 15:07:48 +0200

    NavBar UPDATED

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,78 +1,161 @@
 import React from "react";
-import "./header.css";
+import "./navheader.css";
+// import "./header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Badge from "@mui/material/Badge";
 //import { Container, Navbar, Nav } from "react-bootstrap";
-import { Link ,useNavigate } from "react-router-dom";
-import {useSelector,useDispatch} from 'react-redux'
-import { signin,logout } from "../../actions";
+import { useState, useRef, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { signin, logout } from "../../actions";
 import { chart } from "../../actions";
-import { Button,Badge } from "react-bootstrap";
+// import { Button, Badge } from "react-bootstrap";
 
 const Header = () => {
   let dispatch = useDispatch();
   let navigate = useNavigate();
-  const token = useSelector(state => state.auth) //state of token 
-  const initstate = ()=>{ ////GO BACK ALL STATES TO INIT STATES  
-     dispatch(logout());
-     dispatch(chart());
-     navigate('/')
-  }
-  
-  const handlerouting =(type)=>{
-      if (type === "owner"){navigate ('/appadmin')}
-      else if (type === "user"){navigate ('/user')}
-      else if (type === "c_admin"){navigate('/clinicdoctor')}
-      else if (type === "p_admin"){navigate('/pharmacyadmin')}
-      else if (type === "h_admin"){navigate('/hospitaladmin')}
-      else if (type === "doctor"){navigate('/doctor')}
+  const token = useSelector((state) => state.auth); //state of token
+  const initstate = () => {
+    ////GO BACK ALL STATES TO INIT STATES
+    dispatch(logout());
+    dispatch(chart());
+    navigate("/");
+  };
+  const [showNav, setShowNav] = useState(false);
+  const linksContainerRef = useRef(null);
+  const loginContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const loginRef = useRef(null);
+
+  const handlerouting = (type) => {
+    if (type === "owner") {
+      navigate("/appadmin");
+    } else if (type === "user") {
+      navigate("/user");
+    } else if (type === "c_admin") {
+      navigate("/clinicdoctor");
+    } else if (type === "p_admin") {
+      navigate("/pharmacyadmin");
+    } else if (type === "h_admin") {
+      navigate("/hospitaladmin");
+    } else if (type === "doctor") {
+      navigate("/doctor");
     }
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    const loginHeight = loginRef.current.getBoundingClientRect().height;
+    console.log(linksHeight);
+    if (showNav) {
+      //   const total = linksHeight + loginHeight;
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+      loginContainerRef.current.style.height = `${loginHeight + 30}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+      loginContainerRef.current.style.height = "0px";
+    }
+  }, [showNav]); // so for every time showNav changed useEffect will run
+
   return (
-    <div className="Container">
-      <div className="wrapper">
-        <Link to="/" className="link-custom">
-          <div className="left">
-            <img
-              className="logo-image"
-              alt="logo"
-              src="/assets/images/logo.png"
-            />
-            <h3 className="logo-name">FUTURE MEDICAL</h3>
-          </div>
-        </Link>
-        <div className="center">
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
           <Link to="/" className="link-custom">
-            <button>Home</button>
+            <div className="logo-left">
+              <img
+                className="logo-image"
+                alt="logo"
+                src="/assets/images/Untitled-2.svg"
+              />
+              {/* <h3 className="logo-name">FUTURE MEDICAL</h3> */}
+            </div>
           </Link>
-          <Link to="/" className="link-custom">
-            <button>Specializations</button>
-          </Link>
-          <Link to="/hospitals" className="link-custom">
-            <button>Hospitals</button>
-          </Link>
-          <Link to="/" className="link-custom">
-            <button>About</button>
-          </Link>
-         
-          <Link to="/login" className="link-custom">
-            <button>Contact Us</button>
-          </Link>
+          <button className="nav-toggle" onClick={() => setShowNav(!showNav)}>
+            <i class="bi bi-list"></i>
+          </button>
         </div>
-        { !token.token ? 
-        <div className="right">
-          <Link to="/login">
-            <button>Log in</button>
-          </Link>
-        </div> : <div className="right">
-          
-            <Button style={{backgroundColor:'#06a3da'}} onClick={()=>{handlerouting(token.usertype)}} >
-            Profile <Badge bg="light" style={{color:'black'}}>9</Badge>
-           <span className="visually-hidden">unread messages</span>
-          </Button> 
-                   
-            <button onClick={initstate}>Logout</button>         
-        </div> }
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            <Link to="/" className="link-custom">
+              <li>Home</li>
+            </Link>
+            <Link to="/" className="link-custom">
+              <li>Specializations</li>
+            </Link>
+            <Link to="/hospitals" className="link-custom">
+              <li>Hospitals</li>
+            </Link>
+            <Link to="/" className="link-custom">
+              <li>About Us</li>
+            </Link>
+          </ul>
+        </div>
+        {!token.token ? (
+          <div className="login-container" ref={loginContainerRef}>
+            <ul className="links login-data" ref={loginRef}>
+              <Link to="/login">
+                <li>Log in</li>
+              </Link>
+            </ul>
+          </div>
+        ) : (
+          <div className="login-container" ref={loginContainerRef}>
+            {/* <div className="profile-icon"> */}
+            <ul className="login-data" ref={loginRef}>
+              <li
+                className="profile-icon"
+                onClick={() => {
+                  handlerouting(token.usertype);
+                }}
+              >
+                <div className="icon-circle">
+                  <Badge color="primary" badgeContent={2} showZero>
+                    <AccountCircleIcon />
+                  </Badge>
+                </div>
+              </li>
+              {/* </ul> */}
+              {/* </div> */}
+              {/* <div className="login-container"> */}
+              {/* <ul className="links login-data"> */}
+              {/* <li
+                className="profile-icon"
+                onClick={() => {
+                  handlerouting(token.usertype);
+                }}
+              >
+                <div className="icon-circle">
+                  <Badge color="primary" badgeContent={2} showZero>
+                    <AccountCircleIcon />
+                  </Badge>
+                </div>
+              </li> */}
+              {/* <Button
+                style={{ backgroundColor: "#06a3da" }}
+                onClick={() => {
+                  handlerouting(token.usertype);
+                }}
+              >
+                Profile{" "}
+                <Badge bg="light" style={{ color: "black" }}>
+                  9
+                </Badge>
+                <span className="visually-hidden">unread messages</span>
+              </Button>
+              <AccountCircleIcon /> */}
+              {/* <a id="logout" href="/"> */}
+              <li className="logout" onClick={initstate}>
+                Logout
+              </li>
+              {/* </a> */}
+            </ul>
+            {/* </div> */}
+          </div>
+        )}
       </div>
-    </div>
+    </nav>
 
     // <Navbar bg="light" variant="light">
     //   <Container>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Slider from "../components/Home_Slider/Slider";
-
+import Announcment from "../components/Home_Announcment/Announcment";
 import HighlyRated from "../components/Highly_Rated_DR/HighlyRated";
-import Departmets from '../components/Specialization/Departmets';
+import Departmets from "../components/Specialization/Departmets";
 
 const Home = (props) => {
   return (
     <>
       <Slider />
-      <Departmets  />
+      <Announcment />
+      <Departmets />
       <HighlyRated />
     </>
   );
