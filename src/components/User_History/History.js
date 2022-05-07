6b225b58e5d01339114478ec926467631d7ed862
marina-kamal-mk <74077914+marina-kamal-mk@users.noreply.github.com>
2022-05-08 00:56:49 +0200

    Update History.js

--- src/components/User_History/History.js
+++ src/components/User_History/History.js
@@ -32,7 +32,7 @@ const History = () => {
     try {
       const res = await axios.patch(
         "https://future-medical.herokuapp.com/user/edit/history",
-        { history: history },
+       history,
         config
       );
       alert(res.data);
@@ -41,15 +41,7 @@ const History = () => {
       console.error(err);
     }
   };
-  //edit history
-  const [edit_h, setEdit_h] = useState(false);
-  const [newh, setnewh] = useState(token.history);
-  const setedit_history = () => {
-    Edit_history(newh);
-    token_copy.history = newh;
-    dispatch(signin(token_copy));
-    setEdit_h(false);
-  };
+  
 
   // Floating Buttons Code
   const labels = [
@@ -68,7 +60,7 @@ const History = () => {
 
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
-
+console.log("value", value);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -76,6 +68,39 @@ const History = () => {
   const handleChangeIndex = (index) => {
     setValue(index);
   };
+  //edit flags
+  const [edit_surgeries, setedit_surgeries] = useState(false);
+  const [edit_diseases, setedit_diseases] = useState(false);
+  const [edit_familyhist, setedit_familyhist] = useState(false);
+  const [edit_medications, setedit_medications] = useState(false);
+  const edit_history_flags=()=>{
+    if(value===0) setedit_surgeries(true);
+  else if(value===1) setedit_diseases(true);
+  else if(value===2) setedit_familyhist(true);
+  else if(value===3) setedit_medications(true);
+  }
+  const cancel_edits=()=>{
+    if(value===0) setedit_surgeries(false);
+  else if(value===1) setedit_diseases(false);
+  else if(value===2) setedit_familyhist(false);
+  else if(value===3) setedit_medications(false);
+  }
+
+  //edit history
+  const [news, setnews] = useState(token.history.surgeries);
+  const [newd, setnewd] = useState(token.history.diseases);
+  const [newf, setnewf] = useState(token.history.family_history);
+  const [newm, setnewm] = useState(token.history.medications);
+ 
+  const setedit_history = (e) => {
+    if(value===0) {token_copy.history.surgeries=news; Edit_history({"history.surgeries":news}); setedit_surgeries(false);}
+  else if(value===1) {token_copy.history.diseases=newd; Edit_history({"history.diseases":newd});setedit_diseases(false);}
+  else if(value===2) {token_copy.history.family_history=newf; Edit_history({"history.family_history":newf});setedit_familyhist(false);}
+  else if(value===3) {token_copy.history.medications=newm; Edit_history({"history.medications":newm}); setedit_medications(false); }
+    dispatch(signin(token_copy));
+    
+  };
+  
 
   return (
     <div className="card">
@@ -85,7 +110,7 @@ const History = () => {
           {
             <EditIcon
               style={{ cursor: "pointer" }}
-              onClick={(e) => setEdit_h(true)}
+              onClick={(e) => edit_history_flags()}
             />
           }
         </h3>
@@ -130,34 +155,34 @@ const History = () => {
           >
             <TabPanel value={value} index={0} dir={theme.direction}>
               <p>
-                {edit_h ? (
+                {edit_surgeries ? (
                   <Form.Control
                     as="textarea"
                     placeholder="Update History"
-                    value={newh}
-                    onChange={(e) => setnewh(e.target.value)}
+                    value={news}
+                    onChange={(e) => setnews(e.target.value)}
                     style={{ height: "100px" }}
                   />
                 ) : (
                   //   <input onChange={(e)=>setnewh(e.target.value)}></input>
-                  token.history
+                  token.history.surgeries
                 )}
               </p>
 
-              {edit_h ? (
+              {edit_surgeries ? (
                 //   <Button variant="outline-success" className="col-md-12 text-right" onClick={setedit_history}>Submit</Button>
                 <ButtonGroup>
                   <Button
                     variant="outline-success"
                     className="col-md-12 text-right"
-                    onClick={setedit_history}
+                    onClick={(e)=>setedit_history()}
                   >
                     Submit
                   </Button>
                   <Button
                     variant="outline-danger"
                     className="col-md-12 text-right"
-                    onClick={(e) => setEdit_h(false)}
+                    onClick={(e) => cancel_edits()}
                   >
                     Cancel
                   </Button>
@@ -168,34 +193,34 @@ const History = () => {
             </TabPanel>
             <TabPanel value={value} index={1} dir={theme.direction}>
               <p>
-                {edit_h ? (
+                {edit_diseases ? (
                   <Form.Control
                     as="textarea"
                     placeholder="Update History"
-                    value={newh}
-                    onChange={(e) => setnewh(e.target.value)}
+                    value={newd}
+                    onChange={(e) => setnewd(e.target.value)}
                     style={{ height: "100px" }}
                   />
                 ) : (
                   //   <input onChange={(e)=>setnewh(e.target.value)}></input>
-                  token.history
+                  token.history.diseases
                 )}
               </p>
 
-              {edit_h ? (
+              {edit_diseases ? (
                 //   <Button variant="outline-success" className="col-md-12 text-right" onClick={setedit_history}>Submit</Button>
                 <ButtonGroup>
                   <Button
                     variant="outline-success"
                     className="col-md-12 text-right"
-                    onClick={setedit_history}
+                    onClick={(e)=>setedit_history()}
                   >
                     Submit
                   </Button>
                   <Button
                     variant="outline-danger"
                     className="col-md-12 text-right"
-                    onClick={(e) => setEdit_h(false)}
+                    onClick={(e) => cancel_edits()}
                   >
                     Cancel
                   </Button>
@@ -206,34 +231,34 @@ const History = () => {
             </TabPanel>
             <TabPanel value={value} index={2} dir={theme.direction}>
               <p>
-                {edit_h ? (
+                {edit_familyhist ? (
                   <Form.Control
                     as="textarea"
                     placeholder="Update History"
-                    value={newh}
-                    onChange={(e) => setnewh(e.target.value)}
+                    value={newf}
+                    onChange={(e) => setnewf(e.target.value)}
                     style={{ height: "100px" }}
                   />
                 ) : (
                   //   <input onChange={(e)=>setnewh(e.target.value)}></input>
-                  token.history
+                  token.history.family_history
                 )}
               </p>
 
-              {edit_h ? (
+              {edit_familyhist ? (
                 //   <Button variant="outline-success" className="col-md-12 text-right" onClick={setedit_history}>Submit</Button>
                 <ButtonGroup>
                   <Button
                     variant="outline-success"
                     className="col-md-12 text-right"
-                    onClick={setedit_history}
+                    onClick={(e)=>setedit_history()}
                   >
                     Submit
                   </Button>
                   <Button
                     variant="outline-danger"
                     className="col-md-12 text-right"
-                    onClick={(e) => setEdit_h(false)}
+                    onClick={(e) => cancel_edits()}
                   >
                     Cancel
                   </Button>
@@ -244,34 +269,34 @@ const History = () => {
             </TabPanel>
             <TabPanel value={value} index={3} dir={theme.direction}>
               <p>
-                {edit_h ? (
+                {edit_medications ? (
                   <Form.Control
                     as="textarea"
                     placeholder="Update History"
-                    value={newh}
-                    onChange={(e) => setnewh(e.target.value)}
+                    value={newm}
+                    onChange={(e) => setnewm(e.target.value)}
                     style={{ height: "100px" }}
                   />
                 ) : (
                   //   <input onChange={(e)=>setnewh(e.target.value)}></input>
-                  token.history
+                  token.history.medications
                 )}
               </p>
 
-              {edit_h ? (
+              {edit_medications ? (
                 //   <Button variant="outline-success" className="col-md-12 text-right" onClick={setedit_history}>Submit</Button>
                 <ButtonGroup>
                   <Button
                     variant="outline-success"
                     className="col-md-12 text-right"
-                    onClick={setedit_history}
+                    onClick={(e)=>setedit_history()}
                   >
                     Submit
                   </Button>
                   <Button
                     variant="outline-danger"
                     className="col-md-12 text-right"
-                    onClick={(e) => setEdit_h(false)}
+                    onClick={(e) => cancel_edits()}
                   >
                     Cancel
                   </Button>
