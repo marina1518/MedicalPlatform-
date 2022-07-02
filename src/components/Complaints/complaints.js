efa3c6c9ce878bcd23be4575a9c1b147680c5609
Madonna88 <76403994+Madonna88@users.noreply.github.com>
2022-07-02 08:23:42 +0200

    Complaint_API

--- src/components/Complaints/complaints.js
+++ src/components/Complaints/complaints.js
@@ -11,8 +11,95 @@ import { BsFillTelephoneFill } from "react-icons/bs";
 import {AiFillMessage} from "react-icons/ai";
 import { MdBloodtype, MdLocationOn, MdEmail } from "react-icons/md";
 import "./complaint.css"
+import axios from "axios";
+import { async } from "@firebase/util";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 const Complaint = () => {
-  
+  const token = JSON.parse(useSelector((state) => state.auth));
+  const navigate = useNavigate();
+  const login = () => {
+      navigate("/login");
+    }
+  const complaint_api = () => {
+    //console.log("MADONNAAAA" , data);
+    axios
+    .post('https://future-medical.herokuapp.com/user/complaint',{
+      form : data.complaint,
+      number : data.phone,
+      mail : data.email
+    },{
+      headers: {
+    'Authorization': `Bearer ${token.token}`
+    }})
+    .then((res) => {
+      console.log(res.data);
+      alert("Complaint has been added Successfully");
+    })
+    .catch(function (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+      }
+    });
+  }
+  const data = [
+    {
+      email: "",
+      phone: "",
+      complaint : ""
+    },
+  ];
+  const [email, setEmail] = useState("");
+  const [phone, setphone] = useState("");
+  const [comp, setComp] = useState("");
+  const [e_ph, sete_ph] = useState("");
+  //const [e_u, sete_u] = useState("");
+  const [e_c, sete_c] = useState("");
+  const [e_e, sete_e] = useState("");
+  const submit_value = (e) => {
+    e.preventDefault();
+    var flag = 0;
+    //submit(email,password);
+    // if (username === "") {
+    //   flag = 1;
+    //   sete_u("!! required username");
+    // }
+    if (email === "") {
+      flag = 1;
+      sete_e("!! required Email");
+    }
+    if (phone.length !== 11 && phone != "") {
+      flag = 1;
+      sete_ph("Required 11 numbers");
+      //setflag("true");
+      console.log(flag);
+    }
+    // if (phone === "")
+    // {
+    //   flag=1;
+    //   sete_ph("!! required Phone Number");
+    //   //setflag("true");
+    // }
+    console.log(flag);
+    if (flag === 0) {
+      data.email = email;
+      data.phone = phone;
+      //data.username = username;
+      data.complaint = comp;
+      console.log(data);
+      if(token.token){
+        complaint_api();
+      }
+      else{
+        login();
+      }
+    }
+  };
+  // useEffect(() => {
+  //   complaint_api(email,phone,comp);
+  // }, []);
+ 
   return (
     <div className="login_sigup_container">
       <Container>
@@ -22,7 +109,7 @@ const Complaint = () => {
           </h1>
           <br />
           <div className="Total_form_signup">
-            <Form >
+            <Form onSubmit={submit_value}>
               <div className="form-details_signup2">
                 <div style={{ gridArea: "1 / 1 / 2 / 2" }}>
                   <Form.Group controlId="formBasicusername">
@@ -44,7 +131,12 @@ const Complaint = () => {
                       type="email"
                       placeholder="Enter email"
                       required
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                        sete_e("");
+                      }}
                     />
+                    <h6 style={{ color: "red" }}>{e_e}</h6>
                   </Form.Group>
                 </div>
                 <br />
@@ -56,7 +148,12 @@ const Complaint = () => {
                       placeholder="Phone"
                       type="text"
                       required
+                      onChange={(e) => {
+                        setphone(e.target.value);
+                        sete_ph("");
+                      }}
                     />
+                    <h6 style={{ color: "red" }}>{e_ph}</h6>
                   </Form.Group>
                 </div>
                 <br />
@@ -68,7 +165,12 @@ const Complaint = () => {
                       placeholder="Type your complaint"
                       as="textarea"
                       required
+                      onChange={(e) => {
+                        setComp(e.target.value);
+                        sete_c("");
+                      }}
                     />
+                    <h6 style={{ color: "red" }}>{e_c}</h6>
                   </Form.Group>
                 </div>
                 <br/>
@@ -76,6 +178,7 @@ const Complaint = () => {
                   <Button
                     variant="primary btn-block"
                     type="submit"
+                    onSubmit={submit_value}
                   >
                     Submit
                   </Button>
